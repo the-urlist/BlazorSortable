5d8e6f62d36f7c9db35308ee85a38d2d55806e10
Your Name <youremail@example.com>
2023-12-25 17:55:15 -0600

    Add font awesome and update MainLayout and SortableList

--- Shared/SortableList.razor.js
+++ Shared/SortableList.razor.js
@@ -1,14 +1,23 @@
-export function init(id, component) {
-    new Sortable(document.getElementById(id), { 
+export function init(id, group, handle, component) {
+    new Sortable(document.getElementById(id), {
+        group: group || '',
         animation: 200,
         forceFallback: true,
+        handle: handle || undefined,
         onUpdate: (event) => {
             // Revert the DOM to match the .NET state
             event.item.remove();
             event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);
 
             // Notify .NET to update its model and re-render
-            component.invokeMethodAsync('Drop', event.oldDraggableIndex, event.newDraggableIndex);
+            component.invokeMethodAsync('OnUpdateJS', event.oldDraggableIndex, event.newDraggableIndex);
+        },
+        onRemove: (event) => {
+            event.item.remove();
+            event.from.insertBefore(event.item, event.from.childNodes[event.oldIndex]);
+
+            // Notify .NET to update its model and re-render
+            component.invokeMethodAsync('OnRemoveJS', event.oldDraggableIndex, event.newDraggableIndex);
         }
     });
 }
